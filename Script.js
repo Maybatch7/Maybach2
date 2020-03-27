bd70243b70a651cb39541f6002daf741935a7df5
May Ozohili <bmay7097@gmail.com>
2020-03-27 18:41:45 +0100

    First commit

---
+++ Script.js
@@ -0,0 +1,14 @@
+var name = "Ozohili May";
+
+var myCourses = ["HTML", "CSS", "JAVASCRIPT", "CODING"];
+
+console.log("My name is: " + name +" and I offer these Start.ng Courses: " + myCourses);
+
+
+for (x = 1; x <= 200; x++) {
+
+	if((x % 2) == 0){
+		console.log(x);
+	}
+
+}
